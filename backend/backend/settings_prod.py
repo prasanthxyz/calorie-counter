fae365c5cc2dd620ff64469f20a596489acd2983
Prasanth P <prasanthxyz@gmail.com>
2020-10-16 23:12:05 +0530

    MySql DB setup for PROD

--- backend/backend/settings_prod.py
+++ backend/backend/settings_prod.py
@@ -3,3 +3,14 @@ from .settings import *
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ccdb',
+        'USER': 'dbadmin@calorie-counter-db',
+        'PASSWORD': os.getenv('DB_PASS'),
+        'HOST': 'calorie-counter-db.mysql.database.azure.com',
+        'PORT': '3306',
+    }
+}
