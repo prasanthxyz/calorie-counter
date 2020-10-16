787efa4228d52ee8a5c89125d5a091aff10f7efd
Prasanth P <prasanthxyz@gmail.com>
2020-10-16 21:24:09 +0530

    Updated startup.sh

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,4 +1,4 @@
-cd backend
-pip install -r requirements.txt
+python -m pip install --upgrade pip setuptools wheel
+python -m pip install -r requirements.txt
 python ./manage.py migrate
 gunicorn --bind=0.0.0.0 --timeout 600 --workers=4 backend.wsgi:application --access-logfile '-' --error-logfile '-'
