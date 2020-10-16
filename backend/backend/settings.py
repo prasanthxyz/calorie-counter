d09cc4b3ca57619419fe4a3b1c4369251902e1e4
Prasanth P <prasp@microsoft.com>
2020-10-16 14:32:25 +0530

    prod-settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8)ay9x@d&wj#qzn*$dpqg@qq&=d&&obu0igjfdc!^7k80lhow+'
+SECRET_KEY = 'SECRET_KEY_HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
