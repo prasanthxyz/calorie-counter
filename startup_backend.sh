ddd4445928ccb34fa09de3b4faebe7e2660ef0b5
Prasanth P <prasanthxyz@gmail.com>
2020-10-16 23:26:54 +0530

    apt-get install mysql packages

--- startup_backend.sh
+++ startup_backend.sh
@@ -1,4 +1,5 @@
 cd backend
+sudo apt-get install python3-mysqldb libmysqlclient-dev
 python -m pip install --upgrade pip setuptools wheel
 python -m pip install -r requirements.txt
 python ./manage.py migrate
