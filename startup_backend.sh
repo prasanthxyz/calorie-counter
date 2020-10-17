0d8082ee4c6d35033e523e9dea5fbf1ba2c97750
Prasanth P <prasanthxyz@gmail.com>
2020-10-17 13:19:21 +0530

    Add Azure Config, remove startup_backend sh

--- startup_backend.sh
+++
@@ -1,5 +0,0 @@
-cd backend
-python -m pip install --upgrade pip setuptools wheel
-python -m pip install -r requirements.txt
-python ./manage.py migrate
-gunicorn --bind=0.0.0.0 --timeout 600 --workers=4 backend.wsgi:application --access-logfile '-' --error-logfile '-'
