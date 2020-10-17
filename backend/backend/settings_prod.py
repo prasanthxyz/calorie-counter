ed1114a9a34fbfb0640f4067eea1010ade4edbb3
Prasanth P <prasanthxyz@gmail.com>
2020-10-18 01:49:04 +0530

    Update dockerfile to avoid git clone, debug mode configuration

--- backend/backend/settings_prod.py
+++ backend/backend/settings_prod.py
@@ -1,7 +1,7 @@
 from .settings import *
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = True
+DEBUG = os.getenv('DEBUG_MODE') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
 
 DATABASES = {
