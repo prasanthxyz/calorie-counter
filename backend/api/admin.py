56a775c559696d8da25f6d590527e2193ac08769
Jyothis T S <jyothis.pillai@codeworldwide.com>
2020-10-24 16:19:41 +0530

    introduced food items and serializers

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from api.models import FoodItem, DailyLog
 
-# Register your models here.
+admin.site.register(FoodItem)
+admin.site.register(DailyLog)
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-
-from . import views
+from . import views_user, views_food
 
 urlpatterns = [
-    url(r'^signup$', views.signup, name='signup'),
-    url(r'^login$', views.login, name='login'),
-    url(r'^logout$', views.logout, name='logout'),
-    url(r'^closeaccount$', views.closeaccount, name='closeaccount'),
+    url(r'^signup$', views_user.signup, name='signup'),
+    url(r'^login$', views_user.login, name='login'),
+    url(r'^logout$', views_user.logout, name='logout'),
+    url(r'^closeaccount$', views_user.closeaccount, name='closeaccount'),
+    url(r'^fooditem$', views_food.fooditem, name='fooditem'),
 ]
