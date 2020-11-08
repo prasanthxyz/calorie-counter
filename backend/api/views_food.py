9c1e157a794185ca145779a38b81848b89b756b8
Prasanth P <prasanthxyz@gmail.com>
2020-11-08 22:36:01 +0530

    FoodItem CRUD operations view complete
    - To use DRF ViewSet to do the same

--- backend/api/views_food.py
+++ backend/api/views_food.py
@@ -7,14 +7,29 @@ from api.models import FoodItem
 from api.serializers import FooditemSerializer
 
 
-@api_view(['GET', 'POST', 'DELETE'])
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def fooditem(request):
+    """
+    Create, Retrieve, Update or Delete a FoodItem.
+    """
     if request.method == 'GET':
+        item = request.query_params.get('item')
+        if item:
+            try:
+                food_item = FoodItem.objects.get(user=request.user, item=item)
+                food_serializer = FooditemSerializer(food_item)
+                return Response(food_serializer.data, status=status.HTTP_200_OK)
+            except FoodItem.DoesNotExist:
+                # The specified food item wasn't found, returning all food items instead
+                pass
+
         fooditems = FoodItem.objects.all().filter(user=request.user)
         food_serializer = FooditemSerializer(fooditems, many=True)
         return Response(food_serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'POST':
-        food_info = JSONParser().parse(request)
+
+    food_info = JSONParser().parse(request)
+
+    if request.method == 'POST':
         if 'item' not in food_info or 'calorie' not in food_info:
             return Response({"detail": "item or calorie not provided"}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -31,3 +46,23 @@ def fooditem(request):
             return Response(food_serializer.data, status=status.HTTP_201_CREATED)
         except InvalidOperation:
             return Response({"detail": "Please recheck the values (the calorie should have max 2 digits and max 5 decimal points)"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"detail": "Path not implemented"}, status=status.HTTP_501_NOT_IMPLEMENTED)
+
+    if 'item' not in food_info:
+        return Response({"detail": "item not provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        food_item = FoodItem.objects.get(user=request.user, item=food_info['item'])
+    except FoodItem.DoesNotExist:
+        return Response({"detail": "Food item not found."}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        if 'calorie' not in food_info:
+            return Response({"detail": "calorie not provided"}, status=status.HTTP_400_BAD_REQUEST)
+        food_item.calorie = food_info['calorie']
+        food_item.save()
+        food_serializer = FooditemSerializer(food_item)
+        return Response(food_serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        food_item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
