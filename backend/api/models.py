020b9ef45e91e3dbcc37655bddae6262cd0cad0e
Prasanth P <prasanthxyz@gmail.com>
2020-11-08 20:32:34 +0530

    Model update, and adds functionality to add new FoodItem
    - Removed existing migrations, created new ones
    - Inter table relationships to foreign key now
    - Updated view to post FoodItem

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,17 +4,12 @@ from datetime import date
 
 # Create your models here.
 class FoodItem(models.Model):
-    user = models.OneToOneField(
-        get_user_model(),
-        on_delete=models.CASCADE
-    )
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     item = models.CharField(max_length=200, blank=False, null=False)
     calorie = models.DecimalField(max_digits=7, decimal_places=5, default=0)
 
 
 class DailyLog(models.Model):
-    item = models.ManyToManyField(
-        FoodItem
-    )
+    item = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
     day = models.DateField(default=date.today)
     quantity = models.IntegerField(default=0)
--- backend/api/views_food.py
+++ backend/api/views_food.py
@@ -1,4 +1,6 @@
+from decimal import InvalidOperation
 from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework import status
 from api.models import FoodItem
@@ -11,3 +13,21 @@ def fooditem(request):
         fooditems = FoodItem.objects.all().filter(user=request.user)
         food_serializer = FooditemSerializer(fooditems, many=True)
         return Response(food_serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        food_info = JSONParser().parse(request)
+        if 'item' not in food_info or 'calorie' not in food_info:
+            return Response({"detail": "item or calorie not provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+        item, calorie = food_info['item'], food_info['calorie']
+        try:
+            FoodItem.objects.get(user=request.user, item=item)
+            return Response({"detail": "food item already exists, please modify the existing item"}, status=status.HTTP_400_BAD_REQUEST)
+        except FoodItem.DoesNotExist:
+            pass
+
+        try:
+            food_item = FoodItem.objects.create(user=request.user, item=item, calorie=calorie)
+            food_serializer = FooditemSerializer(food_item)
+            return Response(food_serializer.data, status=status.HTTP_201_CREATED)
+        except InvalidOperation:
+            return Response({"detail": "Please recheck the values (the calorie should have max 2 digits and max 5 decimal points)"}, status=status.HTTP_400_BAD_REQUEST)
